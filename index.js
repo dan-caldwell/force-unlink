bbb4494e7003aaf3e30b8c1dc69565c5e65c83bd
dan-caldwell <dan@caldwell.info>
2025-03-27 23:00:39 -0400

    bug fix

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 const path = require('path');
 const { execFile } = require('node:child_process');
 
+if (!process.argv?.[2]) {
+  console.log('Please provide a package name');
+  process.exit(1);
+}
+
 execFile(path.join(__dirname, './force-unlink.sh'), [process.argv?.[2]], (error, stdout, stderr) => {
   if (stdout) console.log(stdout);
   if (stderr) console.log(stderr);
